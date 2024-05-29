3dbef0ae8d3cea232badbf0b48be6f5d4cebb398
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2024-05-29 23:21:22 +0300

    feat: Initial revision

---
+++ engine/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn add(a: usize, b: usize) -> usize {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
---
+++ sandbox/src/main.rs
@@ -0,0 +1,6 @@
+extern crate engine;
+
+fn main() {
+    let result = engine::add(2, 2);
+    println!("The result of adding 2 and 2 is: {}", result);
+}
